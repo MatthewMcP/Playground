03eab3edddd15ef0460c511de8593b7e16a7e7ff
MatthewMcP <matthew.mcparland@gmail.com>
2018-03-13 09:32:06 +1100

    Adding less to Pokemon MVC

--- PokemonMVCApp/PokemonMVCApp/Controllers/PokemonController.cs
+++ PokemonMVCApp/PokemonMVCApp/Controllers/PokemonController.cs
@@ -1,18 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PokemonMVCApp.Models;
 
 namespace PokemonMVCApp.Controllers
 {
     public class PokemonController : Controller
     {
+
+        Pokemon[] PokemonArray;
+        public PokemonController()
+        {
+            PokemonArray = new Pokemon[]
+        {
+                new Pokemon("Dan", "1"),
+                new Pokemon("Danny", "2"),
+                new Pokemon("Daniel", "3"),
+                new Pokemon("Daniella", "4")
+        };
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View(new Pokemon("Dan", "2"));
+        }
+
+        public IActionResult SearchPokemonName(string name)
+        {
+            return View(PokemonArray[2]);
+        }
+
+        public IActionResult SearchPokemonId(string id)
+        {
+            return View(PokemonArray[3]);
         }
     }
 }
