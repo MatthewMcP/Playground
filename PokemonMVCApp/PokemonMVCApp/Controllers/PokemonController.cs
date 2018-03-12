0cfd3a3daea0619af5ff717c8a9e1e62dc3b0a31
MatthewMcP <matthew.mcparland@gmail.com>
2018-03-12 14:53:48 +1100

    Adding PokemonMVCApp

---
+++ PokemonMVCApp/PokemonMVCApp/Controllers/PokemonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PokemonMVCApp.Models;
+
+namespace PokemonMVCApp.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(new Pokemon("Dan", "2"));
+        }
+    }
+}
---
+++ PokemonMVCApp/PokemonMVCApp/Models/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PokemonMVCApp.Models
+{
+    public class Pokemon
+    {
+        public Pokemon(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+    }
+}
